a83c2090eb9e70a2affbf6d8960442828d510b11
David R. <dreyes@colgate.edu>
2025-04-21 14:08:18 -0400

    Worked on Users, Need DataBase

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -3,16 +3,30 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-    private users = [];
+  private users = []; // TODO: swap for Prisma/DB
 
-    async findOneByEmail(email: string) {
-        return this.users.find(user => user.email === email);
-    }
-    async create(email: string, password: string) {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = { email, password: hashedPassword };
-        this.users.push(newUser);
-        return newUser;
-    }
+    //AuthService.validateUser
+  async findOneByEmail(email: string) {
+    return this.users.find(u => u.email === email);
+  }
+
+  //AuthService.login 
+  async findOneById(id: number): Promise<User | undefined> {
+    return this.users.find(u => u.id === id)
+  }
+
+  //AuthService.signup
+  async create(email: string, password: string) {
+    const hash = await bcrypt.hash(password, 10);
+    const user = { id: Date.now(), email, password: hash };
+    this.users.push(user);
+    return user;
+  }
+
+  /** Expose user list without passwords */
+  async findAllPublic(): Promise<Omit<User, 'password'>[]> {
+    return this.users.map(({ password, ...rest }) => rest)
+  }
 }
+
 
